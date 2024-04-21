71538844829eba6ffaf23f4661e60304fb616ebe
digiteng <digiteng@gmail.com>
2024-04-21 23:00:29 +0300

    Update ee.py

--- ee.py
+++ ee.py
@@ -1,8 +1,7 @@
 from decouple import config
-user="engn"
-password="1234"
-user = config(user, default='')
-password = config(password, default='')
+
+user = config('user', default='')
+password = config('password', default='')
 
 print(user, password)
 
